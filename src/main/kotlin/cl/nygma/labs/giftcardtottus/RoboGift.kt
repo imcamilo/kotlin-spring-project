0de5f414574e76c09da56092549965f54451706a
Camilo <camilo.jorquera.a@gmail.com>
2018-09-01 22:32:30 -0300

    Upload kt files

---
+++ src/main/kotlin/cl/nygma/labs/giftcardtottus/RoboGift.kt
@@ -0,0 +1,11 @@
+package cl.nygma.labs.giftcardtottus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GiftCardTottusApplication
+
+fun main(args: Array<String>) {
+    runApplication<GiftCardTottusApplication>(*args)
+}
