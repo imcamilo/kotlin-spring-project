48497d96db0b6cee725fb6b02a8fb7847dc55d45
Pedro Quintanilla <ext_ppquintanilla@falabella.cl>
2018-10-31 17:03:16 -0300

    create support added

--- src/main/kotlin/cl/nygma/keyproject/service/UsersService.kt
+++ src/main/kotlin/cl/nygma/keyproject/service/UsersService.kt
@@ -1,6 +1,7 @@
 package cl.nygma.keyproject.service
 
 import cl.nygma.keyproject.model.Users
+import org.apache.ibatis.annotations.Param
 
 /**
  * Created by Camilo Jorquera on 29-10-18
@@ -8,5 +9,7 @@ import cl.nygma.keyproject.model.Users
 interface UsersService {
 
     fun findByEmail(email: String): Users
+
+    fun insertParam(users: Users): Unit
 
 }
